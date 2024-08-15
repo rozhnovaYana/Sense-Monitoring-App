382f168474e452f7d8a50db6cd197a97da91d748
rozhnovaYana <scream.yv@gmail.com>
2024-08-15 12:53:51 +0300

    created postgreSQL DB

---
+++ prisma/migrations/20240815092045_init/migration.sql
@@ -0,0 +1,82 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "level" TEXT NOT NULL,
+    "theme" TEXT NOT NULL,
+    "activities" TEXT NOT NULL,
+    "reasons" TEXT NOT NULL,
+    "isResolved" BOOLEAN NOT NULL,
+    "numberOfIncident" TEXT NOT NULL,
+    "numberOfMessage" TEXT NOT NULL,
+    "startDate" TEXT NOT NULL,
+    "endDate" TEXT,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Incident" (
+    "id" SERIAL NOT NULL,
+    "numberOfIncident" TEXT NOT NULL,
+    "timeRequest" TEXT NOT NULL,
+    "timeSend" TEXT NOT NULL,
+    "reporter" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "startDate" TEXT NOT NULL,
+    "isSLA" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Incident_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "parentId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Message_numberOfIncident_key" ON "Message"("numberOfIncident");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Incident_numberOfIncident_key" ON "Incident"("numberOfIncident");
+
+-- AddForeignKey
+ALTER TABLE "Incident" ADD CONSTRAINT "Incident_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="flex mt-20 h-full flex-col items-center justify-center w-full">
-      <h1 className="text-8xl mb-9 text-center font-thin">
-        Sense Monitoring App
-      </h1>
+      <h1 className="text-8xl mb-9 text-center font-thin">Monitoring App</h1>
       <Image width={300} src={img} alt="image" />
     </div>
   );
